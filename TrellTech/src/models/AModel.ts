6e64f44ffbfb983c9ea919c7e635a2b45a408f19
glopez-dev <glopez-dev@proton.me>
2024-02-27 15:22:30 +0100

    feat: Add an abstract class for Models

--- TrellTech/src/models/AModel.ts
+++ TrellTech/src/models/AModel.ts
@@ -1,37 +1,25 @@
 import IModel from "@interfaces/IModel";
+import TrelloID from "types/TrelloID";
 
 abstract class AModel implements IModel {
 
-    id: string;
+    protected _id: TrelloID;
 
     protected APIKey: string = process.env.API_KEY;
     protected APIToken: string = process.env.API_TOKEN;
 
-    /**
-     * Constructor for creating a new instance.
-     *
-     * @param {string} id - the unique identifier
-     */
-    constructor(id: string) {
-        this.setId(id);
-    }
 
     /**
      * Set the ID of the object.
+     * Is called when you assign a new ID to the object.
+     * 
      *
      * @param {string} id - the new ID to set
-     * @return {void} 
      * 
      * @throws {Error} if the ID does not match the regex ^[0-9a-fA-F]{24}$
      */
-    public setId(id: string): void {
-        const idRegex = new RegExp('^[0-9a-fA-F]{24}$');
-
-        if (!idRegex.test(id)) {
-            throw new Error(`ID must match regex ${idRegex}, but got ${id}`);
-        }
-
-        this.id = id;
+    public set id(id: string) {
+        this._id = new TrelloID(id);
     }
 
 
@@ -40,12 +28,12 @@ abstract class AModel implements IModel {
      *
      * @return {string} the ID of the object
      */
-    public getId(): string {
-        return this.id;
+    public get id(): string {
+        return this.id.toString();
     }
 
 
-    public abstract save(model: IModel): IModel;
+    public abstract create(): IModel;
 
     public abstract get(id: string): IModel;
 
