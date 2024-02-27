11384b18f8542db82fc03b5906c2693cb17b61a4
glopez-dev <glopez-dev@proton.me>
2024-02-27 16:03:31 +0100

    feat: Add GluestackUIProvider

--- TrellTech/App.tsx
+++ TrellTech/App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GluestackUIProvider} from '@gluestack-ui/themed';
+import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
   );
 }
 
