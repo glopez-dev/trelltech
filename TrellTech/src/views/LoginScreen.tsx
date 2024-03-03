a5363d8fe0ebfc6399271a7735da19eced100f85
glopez-dev <glopez-dev@proton.me>
2024-03-03 14:28:03 +0100

    config: creates a new project directory for the new architecture

--- TrellTech/src/views/LoginScreen.tsx
+++ TrellTech/src/views/LoginScreen.tsx
@@ -1,17 +1,65 @@
 import { StyleSheet, View, Text } from 'react-native';
+import { 
+  VStack,
+  FormControl,
+  FormControlLabel,
+  FormControlLabelText,
+  FormControlHelper,
+  FormControlHelperText,
+  FormControlError,
+  FormControlErrorText,
+  FormControlErrorIcon,
+  AlertCircleIcon,
+  Input,
+  InputField
+} from '@gluestack-ui/themed';
 
 export default function LoginScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Login Screen</Text>
+        <View style={styles.view}>
+            <VStack space="md">
+              <FormControl>
+                <FormControlLabel mb="$1">
+                  <FormControlLabelText>Username</FormControlLabelText>
+                </FormControlLabel>
+                <Input>
+                  <InputField type="text" placeholder="username" />
+                </Input>
+              </FormControl>
+
+              <FormControl
+                  size="md"
+                  isInvalid={false}
+                  isReadOnly={false}
+                  isRequired={true}
+                >
+                  <FormControlLabel mb="$1">
+                    <FormControlLabelText>Password</FormControlLabelText>
+                  </FormControlLabel>
+                  <Input>
+                    <InputField type="password" placeholder="password" />
+                  </Input>
+                  <FormControlHelper>
+                    <FormControlHelperText>
+                      Must be at least 6 characters.
+                    </FormControlHelperText>
+                  </FormControlHelper>
+                  <FormControlError>
+                    <FormControlErrorIcon as={AlertCircleIcon} />
+                    <FormControlErrorText>
+                      At least 6 characters are required.
+                    </FormControlErrorText>
+                  </FormControlError>
+                </FormControl>
+            </VStack>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
