36ff12b48b2cec44a3244adab67995c1e7d86e72
glopez-dev <glopez-dev@proton.me>
2024-03-04 20:38:58 +0100

    feat( Workspace ): Adds the CRUD methods with associed unit tests.

--- TrellTech/src/api/Workspace.ts
+++ TrellTech/src/api/Workspace.ts
@@ -1,23 +1,46 @@
-import axios from "axios";
-import dotenv from 'dotenv';
-import WorkspaceData from "@src/types/workspaceData";
-dotenv.config();
+import axios, { AxiosResponse } from "axios";
 
-const key = process.env.KEY;
-const token = process.env.TOKEN;
+interface WorkspaceData {
+    /* Mandatory */
+    id: string,
+    displayName: string,
+    /* Optionnal */
+    name?: string,
+    desc?: string,
+    website?: string,
+}
 
-export default class Workspace {
-   
+export default class Workspace implements WorkspaceData {
 
-    constructor(data: WorkspaceData) {
-        for (const key in data) {
-            this[key] = data[key];
-        }
-    }
+    private static APIKey: string = process.env.EXPO_PUBLIC_API_KEY;
+    private static APIToken: string = process.env.EXPO_PUBLIC_API_TOKEN;
+    private static baseURL: string = "https://api.trello.com/1/organizations";
 
-    static async createWorkspace(displayname: string): Promise<Workspace> {
+    /* Mandatory */
+    id: string;
+    displayName: string;
+    /* Optionnal */
+    name?: string;
+    desc?: string;
+    website?: string;
+
+    /**
+     * Static method used to create a new workspace in Trello 
+     * and instanciate a new Workspace model to manipulate it.
+     *
+     * @param {string} displayName - The displayed name of the workspace
+     * @returns {Workspace} The newly created workspace
+     */
+    public static async create(displayName: string): Promise<Workspace> {
+
+        const token = Workspace.APIToken;
+        const key = Workspace.APIKey;
+        const encodedDisplayName = encodeURIComponent(displayName);
+
+        const url = `${Workspace.baseURL}?key=${key}&token=${token}&displayName=${encodedDisplayName}`
+
         try {
-            const response = await axios.post(`https://api.trello.com/1/organizations?displayName=${displayname}&key=${key}&token=${token}`);
+            const response = await axios.post(url);
             return new Workspace(response.data);
         } catch (error) {
             console.error("Error creating workspace:", error.message);
@@ -25,37 +48,85 @@ export default class Workspace {
         }
     }
 
-    async getWorkspaces(id: string) {
+    constructor(data: WorkspaceData) {
+        this.id = data.id;
+        this.displayName = data.displayName;
+        this.name = data.name;
+        this.desc = data.desc;
+        this.website = data.website;
+    }
+
+
+    /**
+     * Method used to update the workspace in Trello
+     *
+     * @returns {Workspace} The updated workspace
+     */
+    public async update(): Promise<Workspace> {
+        const id = this.id;
+
+        const queryParams: string = new URLSearchParams({
+            key: Workspace.APIKey,
+            token: Workspace.APIToken,
+            name: this.name,
+            displayName: this.displayName,
+            desc: this.desc,
+            website: this.website,
+        }).toString();
+
+        const url = `${Workspace.baseURL}/${id}?${queryParams}`
+
         try {
-            const response = await axios.get(`https://api.trello.com/1/organizations/${id}?key=${key}&token=${token}`);
-            return response.data;
+            const response = await axios.put(url);
+
+            return response.status === 200 ? this : null;
+        } catch (error) {
+            console.error("Error updating workspace:", error.message);
+            throw error;
+        }
+    }
+
+    /**
+     * Retrieves a workspace by its ID using the provided parameter.
+     *
+     * @param {string} id - the ID of the workspace to retrieve
+     * @return {Promise<Workspace>} a Promise that resolves with the retrieved workspace
+     */
+    public static async get(id: string): Promise<Workspace> {
+        const baseURL = Workspace.baseURL;
+        const key = Workspace.APIKey;
+        const token = Workspace.APIToken;
+        const url = `${baseURL}/${id}?key=${key}&token=${token}`
+
+        try {
+            const response = await axios.get(url);
+            return new Workspace(response.data);
         } catch (error) {
             console.error("Error getting workspace:", error.message);
             throw error;
         }
     }
 
-    async deleteWorkspace(id: string) {
+    /**
+     * Deletes a workspace by its ID.
+     *
+     * @param {string} id - The ID of the resource to be deleted
+     * @return {Promise<boolean>} A boolean indicating the success of the deletion
+     */
+    public static async delete(id: string): Promise<boolean> {
+        const baseURL = Workspace.baseURL;
+        const key = Workspace.APIKey;
+        const token = Workspace.APIToken;
+        const url = `${baseURL}/${id}?key=${key}&token=${token}`
+
         try {
-            const response = await axios.delete(`https://api.trello.com/1/organizations/${id}?key=${key}&token=${token}`);
-            return response.data;
+            const response = await axios.delete(url);
+            return response.status === 200 ? true : false;
         } catch (error) {
             console.error("Error deleting workspace:", error.message);
             throw error;
         }
     }
 
-    async updateWorkspace(id: string) {
-        try {
-            const response = await axios.put(`https://api.trello.com/1/organizations/${id}?key=${key}&token=${token}`, {
-                displayName: this.displayname
-            });
-            return response.data;
-        } catch (error) {
-            console.error("Error updating workspace:", error.message);
-            throw error;
-        }
-    }
 }
-
 
---
+++ TrellTech/tests/api/Workspace.test.ts
@@ -0,0 +1,32 @@
+import { jest, beforeAll, describe, expect, it } from '@jest/globals';
+import Workspace from '@src/api/Workspace';
+
+
+describe('Workspace class', () => {
+
+    let workspace: Workspace;
+
+    it('should create a new workspace', async () => {
+        workspace = await Workspace.create("My Workspace");
+        expect(workspace.displayName).toBe("My Workspace");
+    });
+
+    it('should update the workspace', async () => {
+        workspace.displayName = "My Updated Workspace";
+        const updatedWorkspace = await workspace.update();
+        expect(updatedWorkspace.displayName).toBe("My Updated Workspace");
+    });
+
+    it('should get the workspace', async () => {
+        const retrievedWorkspace = await Workspace.get(workspace.id);
+        expect(retrievedWorkspace.displayName).toBe("My Updated Workspace");
+    });
+
+    it('should delete the workspace', async () => {
+        const deleted = await Workspace.delete(workspace.id);
+        expect(deleted).toBe(true);
+    });
+
+});
+
+
