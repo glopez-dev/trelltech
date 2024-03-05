1b51627c49aae523a3dab045b9e63daf5a9b4102
glopez-dev <glopez-dev@proton.me>
2024-03-05 14:57:16 +0100

    feat( Tests ): Add tests for the List model.

---
+++ TrellTech/tests/api/List.test.ts
@@ -0,0 +1,38 @@
+import { jest, beforeAll, describe, expect, it } from '@jest/globals';
+import Board from '@src/api/Board';
+import List from '@src/api/List';
+
+
+describe('List class', () => {
+
+    let list: List;
+    let board: Board;
+
+    beforeAll(async () => {
+        board = await Board.create("My Board");
+    });
+
+    it('should create a new List', async () => {
+        list = await List.create("My List", board.id);
+        expect(list.name).toBe("My List");
+        expect(list.idBoard).toBe(board.id);
+    });
+
+    it('should update the List', async () => {
+        list.name = "My Updated List";
+        const updatedList = await list.update();
+        expect(updatedList.name).toBe("My Updated List");
+    });
+
+    it('should get the List', async () => {
+        const retrievedList = await List.get(list.id);
+        expect(retrievedList.name).toBe("My Updated List");
+        expect(retrievedList.id).toBe(list.id);
+    });
+
+    it('should delete the List', async () => {
+        const deleted = await list.delete();
+        expect(deleted).toBe(true);
+    });
+
+});
