b7757be5d61654459f63ae657d5d475378825103
glopez-dev <glopez-dev@proton.me>
2024-03-12 14:23:10 +0100

    refactor( BoardModalCreate.tsx ) Extract the styles in a StyleSheet object.

--- src/components/HomeScreen/BoardModalCreate.tsx
+++ src/components/HomeScreen/BoardModalCreate.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, View, Button, Text, TextInput } from 'react-native';
+import { Modal, View, Button, Text, TextInput, StyleSheet } from 'react-native';
 import Board from '@src/api/Board';
 
 interface BoardModalCreateProps {
@@ -29,15 +29,15 @@ export default function BoardModalCreate(props: BoardModalCreateProps): JSX.Elem
             visible={props.isVisible}
             onRequestClose={props.onClose}
         >
-            <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                <View style={{ backgroundColor: 'white', height: '89%' }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: '#000000' }}>
+            <View style={styles.modal}>
+                <View style={styles.modalContent}>
+                    <View style={styles.modalHeader}>
                         <Button title="Fermer" onPress={props.onClose} />
                     </View>
-                    <View style={{ height: '100%', backgroundColor: '#000000', gap: 10 }}>
-                        <Text style={{ fontSize: 20, marginLeft: 10, color: 'white' }}>Ajouter un tableau</Text>
-                        <View style={{ marginTop: 10, marginBottom: 10, marginLeft: 10, marginRight: 10, padding: 10, backgroundColor: '#1c1c1e', borderRadius: 5, flexDirection: 'row', alignItems: 'center', }}>
-                            <TextInput style={{ flex: 1, color: 'white', }} placeholder="Nom de votre tableau" value={name} onChangeText={handleNameChange} />
+                    <View style={styles.modalBody}>
+                        <Text style={styles.modalBodyText}>Ajouter un tableau</Text>
+                        <View style={styles.modalBodyInput}>
+                            <TextInput style={styles.modalBodyInputText} placeholder="Nom de votre tableau" value={name} onChangeText={handleNameChange} />
                             <Button title="Add" onPress={add} />
                         </View>
                     </View>
@@ -47,3 +47,44 @@ export default function BoardModalCreate(props: BoardModalCreateProps): JSX.Elem
     );
 };
 
+const styles = StyleSheet.create({
+    modal: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        height: '89%',
+    },
+    modalHeader: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        backgroundColor: '#000000',
+    },
+    modalBody: {
+        height: '100%',
+        backgroundColor: '#000000',
+        gap: 10,
+    },
+    modalBodyText: {
+        fontSize: 20,
+        marginLeft: 10,
+        color: 'white',
+    },
+    modalBodyInput: {
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 10,
+        backgroundColor: '#1c1c1e',
+        borderRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    modalBodyInputText: {
+        color: 'white',
+        flex: 1,
+    },
+});
