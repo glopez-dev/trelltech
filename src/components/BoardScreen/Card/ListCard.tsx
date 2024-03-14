badba65d24fc82aa9e28041a08ee9a46e8d39a4c
glopez-dev <glopez-dev@proton.me>
2024-03-14 23:38:34 +0100

    fix( ListCard.tsx ) Removes unused list porp.

--- src/components/BoardScreen/Card/ListCard.tsx
+++ src/components/BoardScreen/Card/ListCard.tsx
@@ -4,11 +4,18 @@ import List from '@src/api/List';
 import { useCardListContext } from './CardListContextProvider';
 
 
+/**
+ * Generates a ListCard component that displays a list of cards.
+ *
+ * @return {JSX.Element} The ListCard component
+ * 
+ * @ATTENTION Depends on the CardListContextProvider to provide access to the list's cards.
+ */
 const ListCard = () => {
 
     const listContext = useCardListContext();
 
-    const renderItem = ({ item }) => (
+    const CardElement = ({ item }) => (
         <View>
             <Text style={{ color: 'white' }}>{item.name}</Text>
         </View>
@@ -17,7 +24,7 @@ const ListCard = () => {
     return (
         <FlatList
             data={listContext.cards}
-            renderItem={renderItem}
+            renderItem={CardElement}
             keyExtractor={(item, index) => index.toString()}
         />
     );
