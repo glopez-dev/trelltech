3e7a72d4f4d4c00e17678ab25bd05a7878515134
Amin Ramdani <mohamed-amin.ramdani@epitech.eu>
2024-03-24 12:04:06 +0100

    feat( BoardCreateModal.tsx ): Add the template choice UI

--- src/components/HomeScreen/BoardModalCreate.tsx
+++ src/components/HomeScreen/BoardModalCreate.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Modal, View, Button, Text, TextInput, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { View, Button, Text, TextInput, StyleSheet, Pressable } from 'react-native';
 import Workspace from '@src/api/Workspace';
-import Board from '@src/api/Board';
 import { useBoardListContext } from './BoardListContext';
+import Modal from 'react-native-modal';
+import { AntDesign } from '@expo/vector-icons';
+import SelectTemplate from './BoardModalCreate/SelectTemplate';
 
 interface BoardModalCreateProps {
     isVisible: boolean;
@@ -12,6 +14,7 @@ interface BoardModalCreateProps {
 
 export default function BoardModalCreate(props: BoardModalCreateProps): JSX.Element {
     const [name, setName] = useState('');
+    const [template, setTemplate] = useState('');
     const context = useBoardListContext();
 
     const handleNameChange = (value: string) => {
@@ -19,6 +22,11 @@ export default function BoardModalCreate(props: BoardModalCreateProps): JSX.Elem
     };
 
     const add = async () => {
+
+        if (name === '') {
+            return;
+        }
+
         const success: boolean = await context.addBoard(name, props.workspace);
 
         if (!success) {
@@ -32,29 +40,47 @@ export default function BoardModalCreate(props: BoardModalCreateProps): JSX.Elem
 
     return (
         <Modal
-            animationType="slide"
-            transparent={true}
-            visible={props.isVisible}
-            onRequestClose={props.onClose}
+            animationIn={"slideInUp"}
+            animationOut={"slideOutDown"}
+            backdropColor='#000'
+            backdropOpacity={0.5}
+            backdropTransitionInTiming={500}
+            isVisible={props.isVisible}
+            onSwipeComplete={props.onClose}
+            swipeDirection={"down"}
+            avoidKeyboard={true}
+            style={{ justifyContent: 'flex-end', margin: 0 }}
         >
-            <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-                    <View style={styles.modal}>
-                        <View style={styles.modalContent}>
-                            <View style={styles.modalHeader}>
-                                <Button title="Fermer" onPress={props.onClose} color="#0c65e3" />
-                            </View>
-                            <View style={styles.modalBody}>
-                                <Text style={styles.modalBodyText}>Ajouter un tableau</Text>
-                                <View style={styles.modalBodyInput}>
-                                    <TextInput style={styles.modalBodyInputText} placeholder="Nom de votre tableau" value={name} onChangeText={handleNameChange} />
-                                    <Button title="Add" onPress={add} color="#0c65e3" />
-                                </View>
-                            </View>
+            <View style={styles.modal}>
+                <View style={styles.modalContent}>
+
+                    <View style={styles.modalHeader}>
+                        <Text style={styles.headerTitle}>Ajouter un tableau</Text>
+                        <AntDesign
+                            name="close"
+                            size={24}
+                            style={styles.closeIcon}
+                            onPress={props.onClose}
+                        />
+                    </View>
+
+                    <View style={styles.modalBody}>
+                        <Text style={{ color: 'black', fontSize: 18, fontWeight: '500' }}>Nom</Text>
+                        <View style={styles.modalBodyInput}>
+                            <TextInput style={styles.modalBodyInputText} placeholder="Nom de votre tableau" value={name} onChangeText={handleNameChange} />
+                        </View>
+
+                        <SelectTemplate template={template} setTemplate={setTemplate} />
+
+                        <View style={styles.modalFooter}>
+                            <Pressable onPress={add} style={styles.button}>
+                                <Text style={styles.buttonText}>Ajouter</Text>
+                            </Pressable>
                         </View>
                     </View>
-                </ScrollView>
-            </KeyboardAvoidingView>
+
+                </View>
+            </View>
         </Modal>
     );
 };
@@ -63,25 +89,38 @@ const styles = StyleSheet.create({
     modal: {
         flex: 1,
         justifyContent: 'flex-end',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        height: '100%',
+        width: '100%',
     },
     modalContent: {
-        backgroundColor: 'white',
-        height: '33%',
+        height: '60%',
+        paddingHorizontal: 20,
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
         overflow: 'hidden',
+        backgroundColor: 'white',
     },
     modalHeader: {
+        paddingVertical: 20,
         flexDirection: 'row',
-        justifyContent: 'flex-end',
-        backgroundColor: '#f3f2f8',
-        padding: 5,
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
+    headerTitle: {
+        paddingLeft: 5,
+        fontSize: 18,
+        fontWeight: '500'
+    },
+    closeIcon: {
+        color: '#0077e6',
+        fontWeight: '500'
+    },
+
     modalBody: {
-        backgroundColor: '#f3f2f8',
         flex: 1,
-        padding: 10,
+        justifyContent: 'flex-start',
+        gap: 10,
+        paddingTop: 5,
     },
     modalBodyText: {
         fontSize: 20,
@@ -89,16 +128,41 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     modalBodyInput: {
+        height: 40,
         flexDirection: 'row',
         alignItems: 'center',
         borderWidth: 1,
+        borderColor: '#172b4c',
+        backgroundColor: '#f0f2f3',
 
         borderRadius: 5,
-        padding: 10,
+        paddingVertical: 2.5,
+        paddingHorizontal: 5,
 
     },
     modalBodyInputText: {
         color: '#172b4c',
         flex: 1,
     },
+    modalFooter: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 40,
+        padding: 10,
+    },
+    button: {
+        backgroundColor: '#0077e6',
+        width: '33%',
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '500'
+    }
 });
